a90471921e1e7b21776a9bd59ca832542b2c0117
PaulHost <paul.hostev@gmail.com>
2016-09-15 22:10:14 +0300

    find prime numbers

---
+++ src/main/PrimeArray.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.ArrayList;
+
+public class PrimeArray {
+
+	private ArrayList<Integer> primesNumbers = new ArrayList<>();
+
+	public ArrayList<Integer> getList() {
+		for (int i = 1; i <= 1000; i++) {
+			int counter = 0;
+
+			for (int j = i; j >= 1; j--) {
+				if (i % j == 0) {
+					counter++;
+				}
+			}
+
+			if (counter == 2) {
+				primesNumbers.add(i);
+			}
+		}
+		return primesNumbers;
+	}
+
+}
